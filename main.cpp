6cd2ee4b82347a00f5703bf75d0faea383e1c7e9
Brunathan <bububg@hotmail.fr>
2014-12-01 10:15:03 +0100

    Init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Brunathan
+ *
+ * Created on 1 décembre 2014, 10:14
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
