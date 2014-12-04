2bd9ec6993e3d049317149dc91fede476664ae25
Oxiumm <oxiumm@gmail.com>
2014-12-04 10:34:58 +0100

    affichage

--- Plateau.cpp
+++ Plateau.cpp
@@ -88,17 +88,12 @@ void Plateau::affichage()
     {
         for(unsigned int j=0;j<taille;j++) // rédaction d'une ligne
         {
-            switch (tab[i][j])
-            {
-                case -1:
-                cout<<"O "; // O correspond au joueur Blanc
-                case 0: 
-                cout<<". "; // . correspond à une case vide
-                case 1:
-                cout<<"X "; // X correspond au joueur Noir
-            }
+            if(tab[i][j]=-1)    cout<<"O "; // O correspond au joueur Blanc
+            if(tab[i][j]=1)     cout<<"X "; // X correspond au joueur Noir
+            if(tab[i][j]=0)     cout<<". "; // . correspond à une case vide
+
         }
         cout<<endl;
-    } 
-}
+    }
+} 
 
